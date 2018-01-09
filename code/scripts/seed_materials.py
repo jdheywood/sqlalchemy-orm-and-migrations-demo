67b5c001796166a12f4262e147d936b46a3c976d
James Heywood <james.heywood@airfinity.com>
2018-01-09 17:42:10 +0000

    Start of script to seed data and unit test to demonstrate orm usage

---
+++ code/scripts/seed_materials.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import uuid
+import os
+import datetime
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
+from sqlalchemy.sql.expression import text
+
+import code.models
+
+
+def _get_engine(conn_id, isolation_level=None):
+    connection_uri = "postgresql://vagrant:vagrant@localhost:5432/demo"
+    if isolation_level:
+        return create_engine(connection_uri, isolation_level=isolation_level)
+
+    return create_engine(connection_uri)
+
+
+def _get_session(engine):
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+
+def seed_materials(session):
+    # Add example models here
+    schleem = models.Schleem(
+        id=uuid4(),
+        height=123,
+        width=456,
+        harvested_at=datetime.datetime.utcnow(),
+        batch='098-qwerty'
+    )
+
+    session.Add(schleem)
+
+    session.commit()
+
+
+if __name__ == '__main__':
+    print('Start...')
+    engine_main = _get_engine("demo")
+    session_main = _get_session(engine_main)
+
+    print('Seeding materials data')
+    seed_materials(session_main)
+
+    session_main.close()
+
+    print('Fin.')
---
+++ code/tests/materials_tests.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# pylint: skip-file
+import unittest
+import datetime
+from uuid import uuid4
+import models
+
+
+class MaterialsTestCase(unittest.TestCase):
+    def test_querying_schleems(self):
+
+        expected_value = None
+
+        actual_value = None
+
+        self.assertEqual(actual_value, expected_value)
